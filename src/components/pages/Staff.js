8ecfc1d488101dc6d3f1c63f2c5536f3c5701a5b
Josh Kardos <joshkardos@gmail.com>
2019-04-04 15:51:04 -0700

    okta react

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { Component }  from 'react'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default () => {
-	return (
+
+class Staff extends Component {
+
+
+	state = {
+		currentUserName: '',
+		currentUserEmail: '',
+		      startDate: new Date()
+
+	}
+
+	componentDidMount() {
+
+		const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+
+		this.setState({
+			currentUserEmail: idToken.idToken.claims.email,
+			currentUserName: idToken.idToken.claims.name,
+		});
+
+
+
+	}
+	handleChange = (date) => {
+    this.setState({
+      startDate: date
+    });
+  }
+
+	render() {
+		console.log(this.state);
+
+		const { currentUserEmail, currentUserName} = this.state;
+		return (
 
 			<div>
-				<h1> Staff </h1>
-			</div>	
 
-		);
-};
+				<h1> Welcome { currentUserName } </h1>
+				<p>Email: { currentUserEmail } </p>
+				<p> You have reached the staff area of the portal </p>
+				<p> Choose a day you would like to request off. </p>
+				<DatePicker 
+
+
+					selected={this.state.startDate}
+        onChange={this.handleChange}/>
+			</div>
+		)
+	}
+
+}
+export default Staff;
